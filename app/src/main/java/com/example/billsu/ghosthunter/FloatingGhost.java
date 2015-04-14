7616e9d2769001a9b02a5eeb5d2ae1950d84b225
su20yu1919 <rs2bd@virginia.edu>
2015-04-14 09:05:29 -0400

    default

---
+++ app/src/main/java/com/example/billsu/ghosthunter/FloatingGhost.java
@@ -0,0 +1,12 @@
+package com.example.billsu.ghosthunter;
+
+/**
+ * Created by AlysonI on 4/12/2015.
+ * this fella floats around randomly
+ */
+public class FloatingGhost extends Ghost {
+
+    public FloatingGhost(double startX, double startY){
+        super(startX, startY);
+    }
+}
